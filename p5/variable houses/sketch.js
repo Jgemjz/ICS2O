642418c5c619615a453504747eb1644c4cfd4812
Jgemjz <lmast1@ocdsb.ca>
2017-03-24 21:28:49 -0400

    Update sketch.js

--- p5/variable houses/sketch.js
+++ p5/variable houses/sketch.js
@@ -4,8 +4,8 @@
 Lucca*/
 
 // variables
-    var baseX=100;
-    var baseY=100;
+    var baseX=52;
+    var baseY=236;
     var houseSize=130;
     // roof variables
         var tri1X=24;
@@ -31,19 +31,29 @@ background(112, 162, 237);
 
 // house1 base
     fill(122, 50, 50);
-    rect(baseX-48, baseY+136, houseSize, houseSize);
+    rect(baseX, baseY, houseSize, houseSize);
 
 // house1 roof
     fill(227, 122, 9);
     triangle(tri1X, tri1Y, tri2X, tri2Y, tri3X, tri3Y);
 
+	baseX=naseX+160;
+	baseY=baseY+186;
+	houseSize=houseSize-50;
+	tri1X=tri1X+210
+	tri1Y=tri1Y+40
+	tri2X=tri2X+150
+	tri2Y=tri2Y+40
+	tri3X=tri3X+180
+	tri3Y=tri3Y+83
+	
 // house2 base
     fill(122, 50, 50);
-    rect(baseX+160, baseY+186, houseSize-50, houseSize-50);
+    rect(baseX, baseY, houseSize, houseSize);
 
 // house2 roof
     fill(227, 122, 9);
-    triangle(tri1X+210, tri1Y+40, tri2X+150, tri2Y+40, tri3X+180, tri3Y+83);
+    triangle(tri1X, tri1Y, tri2X, tri2Y, tri3X, tri3Y);
     
 // sun
     fill(240, 240, 104);
