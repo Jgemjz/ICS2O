cb2f4ee218e700126870193ff692a39fb0452ed9
Lucca <lmast1@ocdsb.ca>
2017-03-30 11:53:18 -0400

    Update sketch.js

--- p5/variable houses/sketch.js
+++ p5/variable houses/sketch.js
@@ -1,19 +1,21 @@
 
  /*houses with variables
-2017-03-23
+2017-03-30
 Lucca*/
 
-// variables
-    var baseX=52;
-    var baseY=236;
-    var houseSize=130;
-    // roof variables
-        var tri1X=24;
-        var tri1Y=254;
-        var tri2X=214;
-        var tri2Y=254;
-        var tri3X=120;
-        var tri3Y=138;
+ // Variables
+    
+        // House Variables
+            var houseX=45;
+            var houseY=220;
+            var houseSize=146;
+        
+        // Roof Variables
+           var roofX=houseX;
+           var roofY=houseY-20;
+           var roofSizeX=146;
+           var roofSizeY=20;
+       
 
 function setup() {
   createCanvas(400, 400);
@@ -23,40 +25,38 @@ function draw() {
  
 noStroke();
 
-background(112, 162, 237);
+   
+    // Background+Grass
+        background
+        (112, 162, 237);
+        fill(38, 138, 40);
+        ellipse(200, 373, 568, 100);
+        
+    // House 1
+        fill(110, 31, 130);
+        rect(houseX, houseY, houseSize, houseSize);
+       
+        // Roof 1
+            fill(217, 156, 70);
+            rect(roofX, roofY, roofSizeX, roofSizeY);
+        
+    // Fixed Variables
+            houseX=houseX+196;
+            houseY=houseY+45;
+            houseSize=houseSize-46;
+            roofX=houseX;
+            roofY=houseY-20;
+            roofSizeX=roofSizeX-45;
+            roofSizeY=roofSizeY;
+        
+    // House 2
+        fill(110, 31, 130);
+        rect(houseX, houseY, houseSize, houseSize);
+        
+        // Roof 2
+            fill(217, 156, 70);
+            rect(roofX, roofY, roofSizeX, roofSizeY);
+            
 
-// grass
-    fill(38, 138, 40);
-    ellipse(200, 373, 568, 100);
 
-// house1 base
-    fill(122, 50, 50);
-    rect(baseX, baseY, houseSize, houseSize);
-
-// house1 roof
-    fill(227, 122, 9);
-    triangle(tri1X, tri1Y, tri2X, tri2Y, tri3X, tri3Y);
-
-	var baseX=naseX+206;
-	var baseY=baseY+51;
-	var houseSize=houseSize-50;
-	var tri1X=tri1X+210;
-	var tri1Y=tri1Y+40;
-	var tri2X=tri2X+150;
-	var tri2Y=tri2Y+40;
-	var tri3X=tri3X+180;
-	var tri3Y=tri3Y+83;
-	
-// house2 base
-    fill(122, 50, 50);
-    rect(baseX, baseY, houseSize, houseSize);
-
-// house2 roof
-    fill(227, 122, 9);
-    triangle(tri1X, tri1Y, tri2X, tri2Y, tri3X, tri3Y);
-    
-// sun
-    fill(240, 240, 104);
-    ellipse(275, 73, 64, 64);
-	
 }
