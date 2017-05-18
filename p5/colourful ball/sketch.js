a07e6859e4091750af3af3c07cd89b3be222df79
Lucca <lmast1@ocdsb.ca>
2017-05-18 12:20:59 -0400

    Add files via upload

---
+++ p5/colourful ball/sketch.js
@@ -0,0 +1,52 @@
+/*rainbow ball moving
+2017-05-18
+Lucca*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+  
+// Variables
+
+    var posX = -50;
+    var posY = 54;
+    var ballSize = 72;
+    
+function draw() {
+    
+    // Background
+    
+        background(145, 65, 65);
+    
+      
+    
+    // Calls
+    
+        ball();
+ };
+
+function ball() {
+        
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipse(posX, posY, ballSize, ballSize);
+        
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipse(posX, posY+72, ballSize, ballSize);
+        
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipse(posX, posY+144, ballSize, ballSize);
+        
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipse(posX, posY+144+72, ballSize, ballSize);
+        
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipse(posX, posY+144+144, ballSize, ballSize);
+        
+        if (posX>430) {
+        posX=-30;
+        }
+        
+        posX+=2; 
+    };
+
+}
