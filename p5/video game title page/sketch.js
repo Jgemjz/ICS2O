fd0b9b8254d433f005b90d75274f4d517b4e8711
Lucca <lmast1@ocdsb.ca>
2017-05-25 13:40:21 -0400

    Update sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -4,6 +4,8 @@ Lucca*/
 
 // Varaibles
 
+    var tX = 200;
+    var tY = 200;
     var posX = 200;
     var posY = -30;
     var potatoX = 200;
@@ -36,9 +38,24 @@ function draw() {
         strokeWeight(1);
         line(308, 40, 374, 40);
 
+    function heart() {
+
+    noStroke();
+    fill(215, 77, 222);
+    triangle(tX+108, tY-156, tX+101, tY-144, tX+114, tY-144);
+    triangle(tX+120, tY-156, tX+114, tY-144, tX+127, tY-144);
+    triangle(tX+113, tY-126, tX+101, tY+-144, tX+127, tY-144);
+
+    };
+    
     // Calls
     
         potato();
+        heart();
+        tX+=28;
+        heart();
+        tX+=29;
+        heart();
 }
 
 function potato() {
