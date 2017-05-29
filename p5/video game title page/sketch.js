10b1324a0f2b64e3879d4498e27ae46c9df29e09
Lucca <lmast1@ocdsb.ca>
2017-05-29 13:27:23 -0400

    Update sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -11,18 +11,18 @@ Lucca*/
     var potatoX = 200;
     var potatoY = 200;
     var potatoSpot = 5;
-    
+
 function setup() {
-    
+
     createCanvas(400, 400);
 }
 
 function draw() {
-    
+
     // Background
-    
+
         background(145, 156, 163);
-    
+
     // Rectangle
 
         stroke(0, 0, 0);
@@ -31,16 +31,15 @@ function draw() {
         rect(290, 8, 101, 68);
 
     // Health
-        
+
         fill(0, 0, 0);
         textSize(21);
         text("Health", 310, 34);
         strokeWeight(1);
         line(308, 40, 374, 40);
 
-    
     // Calls
-    
+
         potato();
         tX=200;
         heart();
@@ -57,16 +56,16 @@ function heart() {
     triangle(tX+108, tY-156, tX+101, tY-144, tX+114, tY-144);
     triangle(tX+120, tY-156, tX+114, tY-144, tX+127, tY-144);
     triangle(tX+113, tY-126, tX+101, tY-144, tX+127, tY-144);
-        
+
     };
-    
+
 function potato() {
-    
+
     // Stuff
 
         stroke(0, 0, 0);
         strokeWeight(0.5);
-
+    
     // Potato
 
         fill(230, 197, 78);
