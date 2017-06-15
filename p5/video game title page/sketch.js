fe203c671c3fcab01db4c9dd510465727c24f884
Lucca <lmast1@ocdsb.ca>
2017-06-15 08:30:51 -0400

    Create sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -326,7 +326,8 @@ function endScreen() {
 	
 		if (bool >= 3) {
 			fill(255, 0, 0);
-			text(num, 200, 200);
+			text("Your score was:", 200, 240);
+			text(num+!, 200, 200);
 			num=num-1;
 		}
  }
