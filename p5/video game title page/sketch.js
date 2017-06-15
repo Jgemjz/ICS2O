2460245f4e1a866e5576a8af7c54419ae18b60a7
Lucca <lmast1@ocdsb.ca>
2017-06-15 08:38:04 -0400

    Create sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -319,16 +319,16 @@ function endScreen() {
 
 		fill(240, 10, 10);
 		textSize(40);
-		text("Game Over", 85, 80);
+		text("Game Over!", 85, 80);
 		textSize(14);
 		text("Click Refresh To play again", 100, 380);
 	
 		if (bool >= 3) {
 			fill(255, 0, 0);
 			textSize(28);
-			text("Your score was:", 150, 160);
-			text(num , 200, 200);
-			text("!", 225, 200);
+			text("Your score was:", 100, 160);
+			text(num , 120, 200);
+			text("!", 200, 200);
 			num=num-1;
 		}
  }
