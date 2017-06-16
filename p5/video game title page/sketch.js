fc4f6f8b372f9262bc07a4e5f20d017fae208b24
Lucca <lmast1@ocdsb.ca>
2017-06-16 15:55:43 -0400

    Update sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -12,7 +12,7 @@ Lucca*/
 
 		var potatoX = 200;
 		var potatoY = 200;
-		var potatoSpot = 5 
+		var potatoSpot = 5;
 
     	// Lava
 
@@ -450,7 +450,7 @@ function endScreen() {
 
 			if (num>9000) {
 			    	textSize(16);
-				text("(OVER 9000!!!)", 140, 280);
+				text("(OVER 9000!!!)", 140, 280)
 			}
 		}
  }
