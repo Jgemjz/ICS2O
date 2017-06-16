72d02e114d64b12dca818f22de737737026450e3
Lucca <lmast1@ocdsb.ca>
2017-06-16 12:11:57 -0400

    Update sketch.js

--- p5/video game title page/sketch.js
+++ p5/video game title page/sketch.js
@@ -338,7 +338,7 @@ function startScreen() {
 	fill(0, 0, 255);
 	textSize(20)
 	text("INSTRUCTIONS:", 40, 40);
-	fill(51, 153, 255);
+	fill(50, 50, 255);
 	textSize(16);
 	text("Mr. Potato has a fear of being cooked alive. Help him avoid his fear by keeping him away from the dripping lava! When hit by a lava ball you'll lose a life indecated by one of the three hearts going from red/pink to black, if all hearts go black you lose, last as long as you can.", 60, 60);
 
