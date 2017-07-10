dca7d9a22d604c24d432f7586b24da041a88b588
Lucca <lmast1@ocdsb.ca>
2017-07-09 21:53:34 -0400

    Add files via upload

---
+++ p5/(Insert Name Here)/sketch.js
@@ -0,0 +1,19 @@
+/* (Insert Project Name Here)
+(Insert Date Here)
+Lucca*/
+
+// Global Variables
+
+
+
+function setup() {
+
+	createCanvas(400, 400);
+}
+
+
+function draw() {
+
+	ellipse(200, 200, 80, 80);
+}
+
