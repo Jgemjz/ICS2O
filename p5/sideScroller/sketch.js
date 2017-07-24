f08ce0d390d026dd21f9f77e2adb7bfe4091a99d
Lucca <lmast1@ocdsb.ca>
2017-07-24 08:32:39 -0400

    Add files via upload

---
+++ p5/sideScroller/sketch.js
@@ -0,0 +1,65 @@
+/* Side Scroller
+
+2017-07-23
+
+Lucca*/
+
+
+
+//Global Variables
+
+
+
+
+
+
+
+function setup() {
+
+
+
+	//(1800, 600)
+	
+
+	
+	createCanvas(400, 400);
+
+}
+
+
+
+function draw() {
+
+	
+	//Backgound
+
+
+		
+		background(140, 140, 140);
+
+	
+	
+	//Calls
+	
+		
+
+		floorPattern();
+
+}
+
+
+
+function floorPattern() {
+
+
+	fill(128, 0, 128);
+	
+	rect(1, 270, 20, 50);
+	
+
+	
+	fill(0, 0, 0);
+	
+	rect(1, 280, 40, 40);
+}
+
